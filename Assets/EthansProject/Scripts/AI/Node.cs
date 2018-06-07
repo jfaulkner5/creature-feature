b0c697d892955dcf08c754ba9673a1a949dde030
SAE\etilley3 <1008464@student.sae.edu.au>
2018-06-07 20:57:43 +1000

    Ethan Projects files added

---
+++ Assets/EthansProject/Scripts/AI/Node.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace EthansProject
+{
+
+    public class Node : MonoBehaviour
+    {
+        public Vector3 spacialInfo;
+
+    }
+}
