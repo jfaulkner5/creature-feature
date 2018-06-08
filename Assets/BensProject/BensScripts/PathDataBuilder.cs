6527efaea04b5bce9c6d625158a2c21695a1b04f
Benjamin Drury <benjamin.m.drury@gmail.com>
2018-06-08 17:57:14 +1000

    Added Scripts, Worked on Node Generation.

---
+++ Assets/BensProject/BensScripts/PathDataBuilder.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BensDroneFleet {
+
+    public class PathDataBuilder : MonoBehaviour {
+
+        public bool build = false;
+        public int xWidth;
+        public int zWidth;
+        [Range(1,10)]
+        public float resolution = 10;
+
+        [Range(0.1f, 1f)]
+        public float nodeHeight;
+
+        [HideInInspector]
+        public Vector3 nodeSize { get { return new Vector3(resolution, nodeHeight, resolution); } }
+
+        [HideInInspector]
+        public List<PathNode> NodeSet = new List<PathNode>();
+        public int NodeCount;
+
+        
+
+        [Header("Gizmo Data")]
+        public bool DisplayGizmo;
+        public Color GizmoColour = Color.cyan;
+
+        private void OnValidate()
+        {
+            if (build)
+            {
+                build = false;
+
+                BuildNodeSet();
+            }
+        }
+
+        void BuildNodeSet()
+        {
+            NodeSet.Clear();
+            
+            for (int x = 0; x < xWidth; x++)
+            {
+                for (int z = 0; z < zWidth; z++)
+                {
+                    NodeSet.Add(MakeNewNode(x,z));
+                }
+            }
+
+            NodeCount = NodeSet.Count;
+        }
+
+        PathNode MakeNewNode(int x, int z)
+        {
+            float newX = x * nodeSize.x;
+            float newZ = z * nodeSize.z;
+
+            PathNode node = new PathNode(newX, newZ);
+            return node;
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            if (DisplayGizmo)
+            {                
+                Gizmos.color = GizmoColour;
+                foreach (PathNode node in NodeSet)
+                {
+                    Gizmos.DrawWireCube(new Vector3(node.x, nodeSize.y * 0.5f, node.z), nodeSize);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/BensProject/BensScripts/Pathing.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BensDroneFleet
+{
+
+    public static class Pathing
+    {
+        /*
+         * The Goal of this class is to return a path.
+         * The simple implimentation will be using basic A*
+         */
+
+        /// <summary>
+        /// The public facing implimentation of the Pathing.
+        /// </summary>
+        /// <param name="startNode">This is where we path from.</param>
+        /// <param name="endNode"> This is where we path to.</param>
+        /// <param name="dataSet"> This is what we path through</param>
+        /// <returns></returns>
+        public static List<PathNode> ReturnPath(PathNode startNode, PathNode endNode, List<PathNode> dataSet)
+        {
+            return AStar(startNode, endNode, dataSet);
+        }
+
+        static List<PathNode> AStar(PathNode startNode, PathNode endNode, List<PathNode> dataSet)
+        {
+            Debug.LogError("A* NotImplimented");
+            return null;
+        }
+    }
+
+    public class PathDataNode
+    {
+        public PathNode node;
+        public PathNode parant;
+        public float x { get { return node.x; } }
+        public float z { get { return node.z; } }
+    }
+
+    [System.Serializable]
+    public class PathNode
+    {
+        public float x;
+        public float z;
+
+        public List<PathNode> nabours = new List<PathNode>();
+        public PathMeta meta;
+
+        public PathNode(float xPos,float zPos)
+        {
+            Debug.Log("newNode");
+            x = xPos;
+            z = zPos;
+        }
+    }
+
+    public class PathMeta
+    {
+        string metaString;
+    }
+}
