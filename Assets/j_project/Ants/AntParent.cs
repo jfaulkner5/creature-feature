c9c06eac229dd862df55083dd80db7d2e6b2728f
Jay <1010100@student.sae.edu.au>
2018-06-11 03:07:20 +1000

    asfdgfh
    sadfgsdf

---
+++ Assets/j_project/Ants/AntParent.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace jfaulkner
+{
+    public class AntParent : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public enum AntState
+        {
+            search,
+            scout,
+            strengthenPath,
+            Hunt,
+
+        }
+    }
+}
---
+++ Assets/j_project/Node.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace jfaulkner
+{
+    public class Node : MonoBehaviour
+    {
+        public Node parent;
+        public bool isPassable;
+        public Vector3 worldPos;
+        public int gridX;
+        public int gridY;
+        public int cost;
+        public int hCost;
+
+        public Node(bool _isPassable, Vector3 _worldPos, int _gridX, int _grixY)
+        {
+            isPassable = _isPassable;
+            worldPos = _worldPos;
+            gridX = _gridX;
+            gridY = _grixY;
+        }
+    }
+}
