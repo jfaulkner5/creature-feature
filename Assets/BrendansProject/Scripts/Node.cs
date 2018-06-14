ad773535e2fd23fb74caa0cf6c039ad6c691d009
Brendan <1010983@student.sae.edu.au>
2018-06-14 12:55:02 +1000

    Pathfinding, Spawning

---
+++ Assets/BrendansProject/Scripts/Node.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace BrendansProject
+{
+    public class Node : IHeapItem<Node>
+    {
+
+        public bool walkable;
+        public Vector3 worldPosition;
+
+        public int gridX, gridY;
+
+        public int gCost, hCost;
+
+        public Node parent;
+
+        public int FCost
+        {
+            get
+            {
+                return gCost + hCost;
+            }
+        }
+
+        public Node(bool _walkable, Vector3 _worldPos, int _gridX, int _gridY)
+        {
+            walkable = _walkable;
+            worldPosition = _worldPos;
+            gridX = _gridX;
+            gridY = _gridY;
+        }
+
+        public int HeapIndex {
+            get {
+                return HeapIndex;
+            }
+            set
+            {
+                HeapIndex = value;
+            }
+        }
+
+        public int CompareTo(Node nodeToCompare) {
+            int compare = FCost.CompareTo(nodeToCompare.FCost);
+            if (compare == 0)
+            {
+                compare = hCost.CompareTo(nodeToCompare.hCost);
+            }
+            return -compare;
+        }
+
+    }
+}
---
+++ Assets/BrendansProject/Scripts/Pathfinding.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;
+
+namespace BrendansProject
+{
+
+    public class Pathfinding : MonoBehaviour
+    {
+
+        public Transform seeker, target;
+
+        LevelGrid levelGrid;
+
+        private void Awake()
+        {
+            levelGrid = GetComponent<LevelGrid>();
+        }
+
+        private void Update()
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                FindPath(seeker.position, target.position);
+            }
+        }
+
+
+        void FindPath(Vector3 startPos, Vector3 targetPos)
+        {
+
+            Stopwatch sw = new Stopwatch();
+
+            Node startNode = levelGrid.NodeFromWorldPoint(startPos);
+            Node targetNode = levelGrid.NodeFromWorldPoint(targetPos);
+
+            Heap<Node> openSet = new Heap<Node>(levelGrid.MaxSize);
+            HashSet<Node> closedSet = new HashSet<Node>();
+
+            openSet.Add(startNode);
+
+            while (openSet.Count > 0)
+            {
+                Node currentNode = openSet.RemoveFirst();
+
+                closedSet.Add(currentNode);
+
+                if (currentNode == targetNode)
+                {
+                    sw.Stop();
+                    print("Path Found in " + sw.ElapsedMilliseconds + "ms");
+
+                    RetracePath(startNode, targetNode);
+                    return;
+                }
+
+                foreach (Node neighbour in levelGrid.GetNeighbours(currentNode))
+                {
+                    if (!neighbour.walkable || closedSet.Contains(neighbour))
+                    {
+                        continue;
+                    }
+
+                    int newMovementCostToNeighbour = currentNode.gCost + GetDistance(currentNode, neighbour);
+                    if (newMovementCostToNeighbour < neighbour.gCost || !openSet.Contains(neighbour))
+                    {
+                        neighbour.gCost = newMovementCostToNeighbour;
+                        neighbour.hCost = GetDistance(neighbour, targetNode);
+                        neighbour.parent = currentNode;
+
+                        if (!openSet.Contains(neighbour))
+                        {
+                            openSet.Add(neighbour);
+                        }
+                    }
+                }
+            }
+        }
+
+
+        private void RetracePath(Node startNode, Node endNode)
+        {
+            List<Node> path = new List<Node>();
+            Node currentNode = endNode;
+
+            while (currentNode != startNode)
+            {
+                path.Add(currentNode);
+                currentNode = currentNode.parent;
+            }
+
+            path.Reverse();
+
+            levelGrid.path = path;
+
+        }
+
+
+        int GetDistance(Node nodeA, Node nodeB)
+        {
+            int distX = Mathf.Abs(nodeA.gridX - nodeB.gridX);
+            int distY = Mathf.Abs(nodeA.gridY - nodeB.gridY);
+
+            if (distX > distY)
+                return 14 * distX + 10 * (distX - distY);
+            return 14 * distX + 10 * (distY - distX);
+
+        }
+
+    }
+}
