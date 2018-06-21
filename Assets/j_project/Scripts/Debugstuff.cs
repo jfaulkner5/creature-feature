8ca61ef9410de572379645643ee6e3a6a9ebc4f0
Jay <1010100@student.sae.edu.au>
2018-06-21 20:38:26 +1000

    rnadom stuff

---
+++ Assets/j_project/Scripts/Debugstuff.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class DebugTool
+{
+    public static void DLog(string _string)
+    {
+        Debug.Log(_string);
+    }
+}
