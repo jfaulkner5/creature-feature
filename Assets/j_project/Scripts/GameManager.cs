e2579e42a5457a0f34dad290a9fda387421fef03
Jay <1010100@student.sae.edu.au>
2018-06-21 20:37:57 +1000

    PAthGrid

--- Assets/j_project/Scripts/GameManager.cs
+++ Assets/j_project/Scripts/GameManager.cs
@@ -20,6 +20,17 @@ namespace jfaulkner
             }
         }
 
+
+
+        PathGrid myPathGrid = new PathGrid();
+        PathFinding myPathFinding = new PathFinding();
+        
+        #region PathData
+
+        public Node[,] levelGrid;
+        public List<Node> path;
+        #endregion
+
         private void Awake()
         {
             if (Instance != null)
@@ -36,39 +47,104 @@ namespace jfaulkner
         // Use this for initialization
         void Start()
         {
-
+            levelGrid = GenerateNodeGrid();
+            path = FindPath();
         }
 
         // Update is called once per frame
         void Update()
         {
-            StartUpStuff();
+
 
         }
 
-        public void StartUpStuff()
+        private void OnDrawGizmos()
         {
-            if (Input.GetKeyDown(KeyCode.Keypad1))
+            if (levelGrid != null)
             {
-
-                Debug.Log("Create grid", this.gameObject);
-
+                foreach (Node node in levelGrid)
+                {
+                    Gizmos.color = (node.isPassable) ? Color.grey : Color.red;
+                    if (path != null)
+                    {
+                        if (path.Contains(node))
+                        {
+                            Gizmos.color = Color.green;
+                        }
+                    }
+                    Gizmos.DrawSphere(node.worldPos, (myPathGrid.nodeDiam / 2));
+                }
             }
-            if (Input.GetKeyDown(KeyCode.Keypad2))
+            else
             {
-                Debug.Log("find path triggered", this.gameObject);
-
-
-            }
-            if (Input.GetKey(KeyCode.Keypad3))
-            {
-
-            }
-            if (Input.GetKey(KeyCode.Keypad4))
-            {
-
+                Debug.Log("levelGrid is null");
             }
 
         }
+
+        public Node[,] GenerateNodeGrid()
+        {
+            myPathGrid.RunSetup();
+            return myPathGrid.GetGrid();
+            //throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// DEBUG Find a path, using a start and end point in the world.
+        /// </summary>
+        private List<Node> FindPath()
+        {
+            Vector3 _start = new Vector3(-10, 0, -10);
+            Vector3 _end = new Vector3(10, 0, 10);
+           return myPathFinding.FindPath(_start, _end);
+        }
+        
+        /// <summary>
+        ///  Find a path, using a start and end point in the world.
+        /// </summary>
+        /// <param name="_startPos"></param>
+        /// <param name="_endPos"></param>
+        public List<Node> FindPath(Vector3 _startPos, Vector3 _endPos)
+        {
+            return myPathFinding.FindPath(_startPos, _endPos);
+        }
+
+        /// <summary>
+        /// Find a path, using a start and end <see cref="Node"/>
+        /// </summary>
+        /// <param name="_startPos"></param>
+        /// <param name="_endPos"></param>
+        public List<Node> FindPath(Node _startPos, Node _endPos)
+        {
+            return myPathFinding.FindPath(_startPos, _endPos);
+        }
+
+        public List<Node> GetNeighbourNodes(Node node)
+        {
+           List<Node> neighbourNodes = new List<Node>();
+            for (int x = -1; x <= 1; x++)
+            {
+                for (int y = -1; y <= 1; y++)
+                {
+                    if (x == 0 && y == 0)
+                    {
+                        continue;
+                    }
+
+                    int nCheckX = node.gridPosX + x;
+                    int nCheckY = node.gridPosY + y;
+
+                    //URGENT unspaghetti
+                    if (nCheckX >= 0 && nCheckX < myPathGrid.gridSize && nCheckY >= 0 && nCheckY < myPathGrid.gridSize)
+                    {
+                        neighbourNodes.Add(levelGrid[nCheckX, nCheckY]);
+                    }
+                }
+            }
+            return neighbourNodes;
+        }
+
     }
+
+
 }
--- Assets/j_project/Scripts/PathGrid.cs
+++ Assets/j_project/Scripts/PathGrid.cs
@@ -7,11 +7,8 @@ using System.Linq;
 namespace jfaulkner
 {
     [System.Serializable]
-    public class PathGrid : MonoBehaviour
+    public class PathGrid
     {
-        //HACK node generator instance
-        public static PathGrid instance;
-
         //Filters the player level to make it easier to find ojects that are considered obsticles
         public LayerMask obstacleMask;
 
@@ -28,57 +25,31 @@ namespace jfaulkner
         //Vector3 testCenter;
         Vector3 worldBotLeft;
 
-        private void OnDrawGizmos()
+
+        public void RunSetup()
         {
-            if (levelGrid != null)
-            {
+            gridWorldSize = new Vector2(20, 20);
+            obstacleMask =~ LayerMask.NameToLayer("obstacleMask");
+            nodeRad = 0.25f;
 
-                foreach (Node node in levelGrid)
-                {
-                    Gizmos.color = (node.isPassable) ? Color.grey : Color.red;
-                    if (path != null)
-                    {
-                        if (path.Contains(node))
-                        {
-                            Gizmos.color = Color.green;
-                        }
-                    }
-                    Gizmos.DrawSphere(node.worldPos, (nodeDiam / 2));
-                }
-            }
-
-        }
-
-
-        public void Start()
-        {
             nodeDiam = nodeRad * 2;
             gridSize = Mathf.RoundToInt(gridWorldSize.x / nodeDiam);
 
-            //HACK stuff for test
-            testMin = _gameObject.GetComponent<Collider>().bounds.min;
-            //testCenter = _gameObject.GetComponent<Collider>().bounds.center;
-            worldBotLeft = testMin;
-            Debug.Log("Unity Editor");
-
-            CreateGrid();
-
-            Vector3 _start = new Vector3(-10, 0, -10);
-            Vector3 _end = new Vector3(10, 0, 10);
-            NewMethod(_start, _end);
+            worldBotLeft = new Vector3(-10,0,-10);
         }
 
-        private static void NewMethod(Vector3 _start, Vector3 _end)
-        {
-            PathFinding.instance.FindPath(_start, _end);
-        }
+        //TODO delete later if needed
+        //private void NewMethod(Vector3 _start, Vector3 _end)
+        //{
+        //    myPathFinding.FindPath(_start, _end);
+        //}
 
 
-        //TODO call from game manager only
-        public void CreateGrid()
+        public Node[,] GetGrid()
         {
-            levelGrid = new Node[gridSize, gridSize];
-            //Vector3 worldBotLeft = new Vector3(0, 0, 0);
+            DebugTool.DLog("Get Grid was called");
+
+            Node[,] newLevelGrid = new Node[gridSize, gridSize];
 
             //TODO add in override for unknown grid size, where start/end point are. q
             for (int x = 0; x < gridSize; x++)
@@ -87,18 +58,20 @@ namespace jfaulkner
                 {
                     Vector3 worldPoint = worldBotLeft + Vector3.right * (x * nodeDiam + nodeRad) + Vector3.forward * (y * nodeDiam + nodeRad);
                     bool passable = !(Physics.CheckCapsule(worldPoint, worldPoint + new Vector3(0, 1, 0), nodeRad, obstacleMask));
-                    //List<Node> neighbours = GetNeighbourNodes(levelGrid[x,y]);
 
-                    levelGrid[x, y] = new Node(passable, worldPoint, x, y);
+                    newLevelGrid[x, y] = new Node(passable, worldPoint, x, y);
+                    //Debug.Log(string.Format("Node added to list with pos {0} and isPassable = {1}",worldPoint,passable));
                 }
             }
+            levelGrid = newLevelGrid;
+            return newLevelGrid;
         }
 
         //TODO | Is it necessary to reverse from node to worldpoint?
         public Node ConvertFromWorldPoint(Vector3 worldPoint)
         {
-            float posX = (worldPoint.x - transform.position.x + gridWorldSize.x / 2) / gridWorldSize.x;
-            float posY = (worldPoint.z - transform.position.z + gridWorldSize.y / 2) / gridWorldSize.y;
+            float posX = (worldPoint.x - /*transform.position.x*/ + gridWorldSize.x / 2) / gridWorldSize.x;
+            float posY = (worldPoint.z - /*transform.position.z*/ + gridWorldSize.y / 2) / gridWorldSize.y;
             posX = Mathf.Clamp01(posX);
             posY = Mathf.Clamp01(posY);
 
@@ -112,145 +85,5 @@ namespace jfaulkner
 
     }
 
-    public class PathFinding
-    {
-        //HACK fix singleton
-        public static PathFinding instance;
-
-        public List<Node> neighbourNodes;
-
-        public List<Node> FindPath(Vector3 startPos, Vector3 finishPos)
-        {
-            Node startNode = PathGrid.instance.ConvertFromWorldPoint(startPos);
-            Node endNode = PathGrid.instance.ConvertFromWorldPoint(finishPos);
-
-            return FindPath(startNode, endNode);
-        }
-
-        public List<Node> FindPath(Node startNode, Node endNode)
-        {
-
-            List<Node> openNodeList = new List<Node>();
-            List<Node> closedNodeList = new List<Node>();
-            List<Node> cameFrom = new List<Node>();
-
-            //NOTE not neccesary unless there is value per path
-
-            openNodeList.Add(startNode);
-
-            Node currentNode = openNodeList[0];
-
-            while (openNodeList != null && openNodeList.Count != 0)
-            {
-
-
-                //HELP Unsure if tests prove difference or needs to be kept outside the while loop
-                //Node currentNode = openSet[0];
-                for (int index = 1; index < openNodeList.Count; index++)
-                {
-                    Debug.Log(index);
-
-                    if (openNodeList[index].F < currentNode.F || openNodeList[index].F == currentNode.F && openNodeList[index].H < currentNode.H)
-                    {
-                        currentNode = openNodeList[index];
-
-
-                    }
-
-                }
-
-                openNodeList.Remove(currentNode);
-                closedNodeList.Add(currentNode);
-
-                if (currentNode == endNode)
-                {
-
-                    currentNode = endNode;
-
-                    while (currentNode != startNode)
-                    {
-                        cameFrom.Add(currentNode);
-                        currentNode = currentNode.parent;
-                    }
-                    cameFrom.Reverse();
-                    return cameFrom;
-                }
-
-                foreach (Node neighbourNode in GetNeighbourNodes(currentNode))
-                {
-
-                    if (!neighbourNode.isPassable || closedNodeList.Contains(neighbourNode))
-                    {
-                        continue;
-                    }
-
-                    float tempCost = currentNode.G + GetDistance(currentNode, neighbourNode);
-
-                    if (tempCost < neighbourNode.G || !openNodeList.Contains(neighbourNode))
-                    {
-                        //URGENT fix code to be more fluid
-                        neighbourNode.G = tempCost;
-                        neighbourNode.H = GetDistance(neighbourNode, endNode);
-                        neighbourNode.parent = currentNode;
-
-                        if (!openNodeList.Contains(neighbourNode))
-                        {
-                            openNodeList.Add(neighbourNode);
-                        }
-                    }
-
-
-                }
-
-
-            }
-
-            return null;
-        }
-
-        //private static int GetDistance(Node currentNode, Node neighbour)
-        //{
-        //    throw new NotImplementedException();
-        //}
-
-        int GetDistance(Node nodeA, Node nodeB)
-        {
-            int dstX = Mathf.Abs(nodeA.gridPosX - nodeB.gridPosX);
-            int dstY = Mathf.Abs(nodeA.gridPosY - nodeB.gridPosY);
-
-            if (dstX > dstY)
-            {
-                return 14 * dstY + 10 * dstX - dstY;
-            }
-            else
-            {
-                return 14 * dstX + 10 * dstY - dstX;
-            }
-        }
-
-        List<Node> GetNeighbourNodes(Node node)
-        {
-            neighbourNodes = new List<Node>();
-            for (int x = -1; x <= 1; x++)
-            {
-                for (int y = -1; y <= 1; y++)
-                {
-                    if (x == 0 && y == 0)
-                    {
-                        continue;
-                    }
-
-                    int nCheckX = node.gridPosX + x;
-                    int nCheckY = node.gridPosY + y;
-
-                    if (nCheckX >= 0 && nCheckX < PathGrid.instance.gridSize && nCheckY >= 0 && nCheckY < PathGrid.instance.gridSize)
-                    {
-                        neighbourNodes.Add(PathGrid.instance.levelGrid[nCheckX, nCheckY]);
-                    }
-                }
-            }
-            return neighbourNodes;
-        }
-
-    }
+    
 }
