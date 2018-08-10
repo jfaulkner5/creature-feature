44fe21896744d26dec690df85a0ba4e175e5e7dc
Ethan Tilley <1008464@student.sae.edu.au>
2018-08-10 18:20:19 +1000

    time - day & night cycle is le workin

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -3,31 +3,50 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
+#region In Game Time
+    [Header("In Game Time")]
 
-    [Tooltip("The in-game time in 24 hour time"), Header("In Game Time")]
-    public int time = 0000;
+    public Light gameSun;
+    public float dayLength = 300;
 
-    public Transform directionalLight;
+    [Range(0, 1)]
+    public float currentTimeOfDay = 0, morning = 0.23f, night = 0.75f;
 
-    public float timerRate, daySpeed = 1;
-    protected Quaternion directionRotation;
-	// Use this for initialization
-	void Start () {
-        StartCoroutine(Timer());
-	}
-	public IEnumerator Timer()
+    public float timeRate = 1;
+
+    float sunInitIntensity;
+
+    private void Start()
     {
-        yield return new WaitForSeconds(timerRate);
-        TickTime();
+        sunInitIntensity = gameSun.intensity;
     }
-	// Update is called once per frame
-	void Update () {
-        directionRotation.x += Time.deltaTime * daySpeed;
-        directionalLight.rotation = directionRotation;
-	}
 
-    public void TickTime()
+
+    private void Update()
     {
+        UpdateSun();
+
+        currentTimeOfDay += (Time.deltaTime / dayLength) * (timeRate);
+
+        if (currentTimeOfDay >= 1)
+            currentTimeOfDay = 0;
 
     }
+
+    void UpdateSun()
+    {
+        gameSun.transform.localRotation = Quaternion.Euler((currentTimeOfDay * 360f) - 90, 170, 0);
+        float intensityMultiplier = 1;
+
+        if (currentTimeOfDay <= morning || currentTimeOfDay >= night)        
+            intensityMultiplier = Mathf.Lerp(intensityMultiplier, 0, 1 * Time.deltaTime);
+        else if (currentTimeOfDay < morning)
+            intensityMultiplier = Mathf.Clamp01(Mathf.Lerp(intensityMultiplier, (currentTimeOfDay - morning) * (1 / 0.02f), 1 * Time.deltaTime));
+        else if (currentTimeOfDay > night)
+            intensityMultiplier = Mathf.Clamp01(Mathf.Lerp(intensityMultiplier, (1 - ((currentTimeOfDay - night) * (1 / 0.02f))), 1 * Time.deltaTime)); 
+        
+        gameSun.intensity = sunInitIntensity * intensityMultiplier;
+    }
+
+#endregion
 }
